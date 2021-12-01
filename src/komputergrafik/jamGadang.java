7e79029a6fd93566c8ffb02853e2cdaaa7ef7003
triarta123 <agustus.arta22@gmail.com>
2021-12-01 10:48:36 +0700

    commit

--- src/komputergrafik/jamGadang.java
+++ src/komputergrafik/jamGadang.java
@@ -7,12 +7,19 @@ import javax.swing.*;
 public class jamGadang extends JPanel{
     public jamGadang() {
         this.setPreferredSize(new Dimension(800,600));
-        this.setBackground(Color.WHITE);
+        Color a = new Color(255,234,208);
+        this.setBackground(a);
     }
     
     public void paintComponent(Graphics g) {
         super.paintComponent(g);
          
+        
+        Color b = new Color(233,150,122);
+        g.setColor(b);
+        int[] a = new int[]{20,50,80,80,50,20};
+        int[] b = new int[]{70,40,70,110,80,110};
+        g.rect(a,b);
         
           
     }
