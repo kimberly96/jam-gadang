86ee6b2be8d50ae2d74cc047028cca5717a36f5f
triarta123 <agustus.arta22@gmail.com>
2021-12-01 10:54:04 +0700

    commit

--- src/komputergrafik/jamGadang.java
+++ src/komputergrafik/jamGadang.java
@@ -17,9 +17,7 @@ public class jamGadang extends JPanel{
         
         Color b = new Color(233,150,122);
         g.setColor(b);
-        int[] a = new int[]{20,50,80,80,50,20};
-        int[] b = new int[]{70,40,70,110,80,110};
-        g.rect(a,b);
+        g.drawOval(WIDTH, WIDTH, WIDTH, WIDTH);
         
           
     }
