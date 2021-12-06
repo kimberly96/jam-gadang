27ace6177d4ef68b2685e62781e224da77a1fa18
Febriani Felicitas D <febrianifdamanik@gmail.com>
2021-12-06 17:12:19 +0700

    clock
    clock

--- src/komputergrafik/jamGadang.java
+++ src/komputergrafik/jamGadang.java
@@ -1,14 +1,29 @@
 package komputergrafik;
 import java.awt.*;
-import java.awt.event.*;
 import java.awt.geom.*;
 import javax.swing.*;
+import java.awt.event.*;
+import static java.lang.Math.*;
+import java.time.LocalTime;
 
 public class jamGadang extends JPanel{
+    
+   final float degrees06 = (float) (PI / 30);
+   final float degrees30 = degrees06 * 5;
+   final float degrees90 = degrees30 * 3;
+ 
+   final int diameter = 80;
+   final int titikX = 40;
+   final int titikY = 40;
+   
     public jamGadang() {
         this.setPreferredSize(new Dimension(650,750));
         Color bgColor = new Color(247, 241, 208);
         this.setBackground(bgColor);
+        
+        new Timer(1000, (ActionEvent e) -> {
+         repaint();
+      }).start();
     }
     
     public void paintComponent(Graphics g) {
@@ -23,7 +38,7 @@ public class jamGadang extends JPanel{
         g2.setStroke(new BasicStroke(2));
         
         // Draw Oval
-        g.drawOval(140, 175, 370, 370);
+        g.drawOval(100, 175, 450, 450);
         
         // Set All of Object Color
         Color c = new Color (31, 30, 29);
@@ -46,68 +61,141 @@ public class jamGadang extends JPanel{
         QuadCurve2D.Double curve6 = new QuadCurve2D.Double(270,30,260,60,305,80);
         g2.draw(curve6);
         
-        //bangunan yang ada pintu
+        // Draw bangunan yang ada pintu
         g.drawRect(290, 100, 70, 60);
         g2.drawRoundRect(310, 130, 30, 50, 30, 20); 
         
+        // Draw balkon
         g.setColor(new Color(247, 241, 208));
         g.fillRect(260, 160, 130, 30);
         g.setColor(Color.black);
-       // g.drawRect(260, 160, 130, 30);
+        g.drawRect(260, 160, 130, 30);
         
+        // Draw garis balkon
+        for (int i=1; i<=3; i++) {
+            g.drawLine(260+(i*32), 160, 260+(i*32), 190);
+        }      
         
-        //kotak
-        for (int i = 0; i <=90; i=i+25) {
-            g.drawRect (275+i,160,25,25);
-   
-       }
-        //g.drawRect(275,160,25,25);
-        //g.drawRect(300,160,25,25);
-        //g.drawRect(325,160,25,25);
-        //g.drawRect(350,160,25,25);
+        // Draw kotak jam
+        g.drawRect(275,190,100,100);
         
+        // Draw kotak1
+        g.drawRect(265,290,120,60);
         
-        
-        //kotak2
-        g.drawRect(275,185,100,100);
-        g.drawLine(270, 185, 380, 185);
-        
-        //kotak3
-        g.drawRect(275,285,100,70);
-        g.drawRect(320,295,15,25);
-        g.drawRect(320,320,15,25);
-        g.drawLine(270, 285, 380, 285);
-        
-        //kotak4
-        g.drawRect(275,355,100,70);
-        
-        g.drawRect(295,365,15,25);
-        g.drawRect(295,390,15,25);
-        g.drawLine(270, 285, 380, 285);
-        
-        
-        g.drawRect(320,365,15,25);
-        g.drawRect(320,390,15,25);
+        // Draw jendela1
+        for (int i=1; i<=2; i++) {
+            g.drawRect(265+(i*35),300,15,20);
+            g.drawRect(265+(i*35),320,15,20);
+        } 
        
-        g.drawRect(345,365,15,25);
-        g.drawRect(345,390,15,25);
-        g.drawLine(270, 355, 380, 355);
+        // Draw kotak2
+        g.drawRect(255,350,140,60);
+
+        // Draw jendela2
+        for (int i=1; i<=3; i++) {
+            g.drawRect(255+(i*32),360,15,20);
+            g.drawRect(255+(i*32),380,15,20);
+        } 
+               
+        // Draw kotak3
+        g.drawRect(250,410,150,80);
         
-        //kotak5
+        // Draw jendela3
+        for (int i=1; i<=3; i++) {
+            g.drawRect(250+(i*34),430,15,20);
+            g.drawRect(250+(i*34),450,15,20);
+        } 
         
-        g.drawRect(265,425,125,90);
+        // Draw garis pembagi kotak kotak
+        int y = 290;
+        for (int i=0; i<3; i++) {
+            g.drawLine(260-(i*10), y, 390+(i*10), y);
+            y = y+60;
+        } 
         
-        g.drawLine(255, 425, 400, 425);
+        // Draw gerbang1
+        Polygon p1 = new Polygon();
+        p1.addPoint(230,480);//kiri atas
+        p1.addPoint(230,530);//kiri bawah
+        p1.addPoint(280,530);//kanan bawah
+        p1.addPoint(305,490);//kanan atas
+        p1.addPoint(305,480);//kanan atas
+        g.setColor(new Color(247, 241, 208));
+        g.fillPolygon(p1);
+        g.setColor(Color.black);
+        g.drawPolygon(p1);
+
         
-        g.drawRect(320,435,20,35);
-        g.drawRect(320,470,20,35);
+        // Draw gerbang2
+        Polygon p2 = new Polygon();
+        p2.addPoint(420,480);//kiri atas
+        p2.addPoint(420,530);//kiri bawah
+        p2.addPoint(370,530);//kanan bawah
+        p2.addPoint(345,490);//kanan atas
+        p2.addPoint(345,480);//kanan atas
+        g.setColor(new Color(247, 241, 208));
+        g.fillPolygon(p2);
+        g.setColor(Color.black);
+        g.drawPolygon(p2);
         
-        g.drawRect(285,435,20,35);
-        g.drawRect(285,470,20,35);
+        // Draw gerbang4
+        for (int i=0; i<2; i++) {
+            g.drawRect(230,530+(i*20),50,20);
+        } 
         
-        g.drawRect(355,435,20,35);
-        g.drawRect(355,470,20,35);
+        // Draw gerbang5
+        for (int i=0; i<2; i++) {
+            g.drawRect(230,530+(i*20),50,20);
+        } 
         
-          
+        // Draw gerbang6
+        for (int i=0; i<2; i++) {
+            g.drawRect(370,530+(i*20),50,20);
+        } 
+        
+        g.drawLine(180,570,470,570);
+
+        // Draw pintu
+        QuadCurve2D.Double pintu = new QuadCurve2D.Double(300,570,325,460,350,570);
+        g2.draw(pintu);
+        
+        // Draw Clock
+        Graphics2D gc = (Graphics2D) g;
+        gc.setRenderingHint(RenderingHints.KEY_ANTIALIASING,RenderingHints.VALUE_ANTIALIAS_ON);
+ 
+        drawFace(gc);
+
+        final LocalTime time = LocalTime.now();
+        int hour = time.getHour();
+        int minute = time.getMinute();
+        int second = time.getSecond();
+
+        float angle = degrees90 - (degrees06 * second);
+        drawHand(gc, angle, 120 / 2 - 30, Color.red);
+
+        float minsecs = (minute + second / 60.0F);
+        angle = degrees90 - (degrees06 * minsecs);
+        drawHand(gc, angle, diameter / 3 + 10, Color.black);
+
+        float hourmins = (hour + minsecs / 60.0F);
+        angle = degrees90 - (degrees30 * hourmins);
+        drawHand(gc, angle, diameter / 4 + 10, Color.black);
+        
     }
+    
+    private void drawFace(Graphics2D g) {
+      g.setStroke(new BasicStroke(2));
+      g.setColor(new Color(247, 241, 208));
+      g.translate(285,200);
+      g.fillOval(0, 0, diameter, diameter);
+      g.setColor(Color.black);
+      g.drawOval(0, 0, diameter, diameter);
+   }
+ 
+   private void drawHand(Graphics2D g, float angle, int radius, Color color) {
+      int x = titikX + (int) (radius * cos(angle));
+      int y = titikY - (int) (radius * sin(angle));
+      g.setColor(color);
+      g.drawLine(titikX, titikY, x, y);
+   }
 }
